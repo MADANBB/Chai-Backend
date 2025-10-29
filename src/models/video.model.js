8ba2b2445209dd977f8a01a3ac81582c1ddbd257
Madan B B <Madanbb2004@gmail.com>
2025-10-29 06:00:13 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        duration: {
+            type: Number, //in seconds
+            required: true,
+        },
+        isPublised: {
+            type: Boolean,
+            default: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    { timestamps: true},
+)
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
+
